719458fda44a0b65ca8e39e118c949ff7c9a5465
ANTONIOS-MAJ-HE <antonio.santiago@tecsup.edu.pe>
2024-04-04 15:04:09 -0500

    puertos 2

--- stech_main/settings.py
+++ stech_main/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-97o&+knmnla0dlpqqa3s_(-kxd(%x8#f@u2@d_holbr@jetu_n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'reporte-b-stech.onrender.com']
 
 
 # Application definition
